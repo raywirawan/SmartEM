b81b2adc1bfe2d2e7e0bfa7dae5c097da000b6bb
Ray <ray.wirawan@Gmail.com>
2020-03-01 22:13:19 +0700

    First Commit

---
+++ app/src/main/java/com/iotproject/smartem/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iotproject.smartem.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iotproject.smartem.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
